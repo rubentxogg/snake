2838a49c678b87b4d19b7418bfa9aeb26dfb4f11
rubentxogg <rubentxogg@hotmail.com>
2022-06-04 11:35:57 +0200

    [ADD] Collision apple detection

--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ var tileCount = 20;
 var tileSize = canvas.width / tileCount - 2;
 var headX = 10;
 var headY = 10;
+var appleX = 5;
+var appleY = 5;
 var xVelocity = 0;
 var yVelocity = 0;
 function drawGame() {
     clearScreen();
     changeSnakePosition();
+    checkAppleCollision();
+    drawApple();
     drawSnake();
     setTimeout(drawGame, 1000 / speed);
 }
@@ -23,6 +27,16 @@ function drawSnake() {
     ctx.fillStyle = "orange";
     ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 }
+function checkAppleCollision() {
+    if (appleX === headX && appleY === headY) {
+        appleX = Math.floor(Math.random() * tileCount);
+        appleY = Math.floor(Math.random() * tileCount);
+    }
+}
+function drawApple() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
+}
 function changeSnakePosition() {
     headX = headX + xVelocity;
     headY = headY + yVelocity;
@@ -31,21 +45,33 @@ document.addEventListener('keydown', keyDown);
 function keyDown(event) {
     // Up
     if (event.keyCode === 38) {
+        if (yVelocity === 1) {
+            return;
+        }
         yVelocity = -1;
         xVelocity = 0;
     }
     // Down
     if (event.keyCode === 40) {
+        if (yVelocity === -1) {
+            return;
+        }
         yVelocity = 1;
         xVelocity = 0;
     }
     // Left
     if (event.keyCode === 37) {
+        if (xVelocity === 1) {
+            return;
+        }
         yVelocity = 0;
         xVelocity = -1;
     }
     // Right
     if (event.keyCode === 39) {
+        if (xVelocity === -1) {
+            return;
+        }
         yVelocity = 0;
         xVelocity = 1;
     }
--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,17 @@ let tileSize: number = canvas.width / tileCount - 2;
 let headX: number = 10;
 let headY: number = 10;
 
+let appleX: number = 5;
+let appleY: number = 5;
+
 let xVelocity: number = 0;
 let yVelocity: number = 0;
 
 function drawGame(): void {
   clearScreen();
   changeSnakePosition();
+  checkAppleCollision();
+  drawApple();
   drawSnake();
   setTimeout(drawGame, 1000 / speed);
 }
@@ -31,6 +36,18 @@ function drawSnake(): void {
   ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 }
 
+function checkAppleCollision(): void {
+  if(appleX === headX && appleY === headY) {
+    appleX = Math.floor(Math.random() * tileCount);
+    appleY = Math.floor(Math.random() * tileCount);
+  }
+}
+
+function drawApple(): void {
+  ctx.fillStyle = "red";
+  ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize); 
+}
+
 function changeSnakePosition(): void {
   headX = headX + xVelocity;
   headY = headY + yVelocity;
@@ -41,24 +58,36 @@ document.addEventListener('keydown', keyDown);
 function keyDown(event: { keyCode: number }): void {
   // Up
   if(event.keyCode === 38) {
+    if(yVelocity === 1) {
+      return;
+    }
     yVelocity = -1;
     xVelocity = 0;
   }
 
   // Down
   if(event.keyCode === 40) {
+    if(yVelocity === -1) {
+      return;
+    }
     yVelocity = 1;
     xVelocity = 0;
   }
 
   // Left
   if(event.keyCode === 37) {
+    if(xVelocity === 1) {
+      return;
+    }
     yVelocity = 0;
     xVelocity = -1;
   }
 
   // Right
   if(event.keyCode === 39) {
+    if(xVelocity === -1) {
+      return;
+    }
     yVelocity = 0;
     xVelocity = 1;
   }
